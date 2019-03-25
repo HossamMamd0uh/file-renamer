b79b17a3732eb086a5d5bb68a3140d47135bfd1b
HossamMamd0uh <hossammamdouh8010@gmail.com>
2019-03-25 02:37:25 +0200

    just renaming

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # file-renamer
-An one click file renamer
+- An one click file renamer
+- Copy the file into the desired directory
+- Open a terminal and type "python3 rename.py"
+- Enjoy
---
+++ rename.py
@@ -0,0 +1,18 @@
+
+import os
+
+#Renames the multiple file within the same directory with appending number
+
+
+#Replace os.getcwd with file path
+
+path =  os.getcwd()
+files = os.listdir(path)
+i = 1
+
+for file in files:
+    filename, file_extension = os.path.splitext(file)
+    
+    #Replace hello with the name you want
+    os.rename(os.path.join(path, file), os.path.join(path, "hello" + str(i) + file_extension))
+    i = i+1
